f4b5ec07fd00ceb5d4f238cdf57cacd17628ca14
Rodi420 <rodrigo.viamontes@outlook.com>
2023-11-14 17:31:53 +0100

    Update v2.8
    Version 2.8
    
    roulette.sh:
    - adjusted comments
    - adjusted all if and moved then/do to previous line
    - compacted some log code
    - removed double code
    
    roulette_func.sh:
    - adjusted comments
    - adjusted all if and moved then/do to previous line
    - remade loop code
    - fixed compatability for "even" bets
    - fixed #3 by adding an active counter
    - removed logging for now (20x faster)
    - added some additional stuff

--- roulette.sh
+++ roulette.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#rodrigo tavares
-#version 2.7.1
-#overhaul of entire script. remade from scratch with options, dynamic variables, modes and logging.
+# Rodrigo Tavares
+# Version 2.8
+# Overhaul of entire script. remade from scratch with options, dynamic variables, modes and logging.
 
-#static variables
+# Static variables
 #global_budget=1000
 #global_wager=10
 #global_turns=1000
@@ -16,29 +16,28 @@ global_delimiter="-"
 
 
 
-#make log directory if it doesnt exist
-if [[ ! -d roulette_logs ]]
-then
+# Make log directory if it doesnt exist
+if [[ ! -d roulette_logs ]]; then
     mkdir roulette_logs
 	echo "$global_delimiter" >> "$global_logfile"
     echo "info: logs directory does not exist. creating it..." >> "$global_logfile"
-#if it exists write to log
+# If it exists write to log
 else
 	echo "$global_delimiter" >> "$global_logfile"
 	echo "info: logs directory exists already." >> "$global_logfile"
 fi
 
-#start
+# Start
 clear
 echo -e "Willkommen bei Roulette. Definiere deine Standardwerte:"
 
-#define variables
+# Define variables
 read -r -p "Dein Budget: " global_budget
 read -r -p "Dein Einsatz pro Spiel: " global_wager
 read -r -p "Wie viel mal willst du spielen? " global_turns
 global_budgetOri=$global_budget
 
-#write log
+# Write log
 {
 echo "$global_delimiter"
 echo "chosen values:" 
@@ -49,38 +48,38 @@ echo "$global_delimiter"
 } >> "$global_logfile"
 clear
 
-#performance test
+# Ask for performance test
 echo -e "Willst du einen Performance Test durchführen?\n1. Ja\n2. Nein"
 read -r -p "Eingabe: " global_perfTest
 
-if [[ $global_perfTest -eq 1 ]]
-then
-	#do perf test
+# Do perf test
+if [[ $global_perfTest -eq 1 ]]; then
 	func_perfTest
 	global_perfTestDone=1
 	echo "performance Test done. Resuming..." >> "$global_logfile"
 else
 	echo "performance Test not done. Resuming..." >> "$global_logfile"
-	#this variable value is called nowhere**
+	# This variable value is called nowhere**
 	global_perfTestDone=0
 fi
 
-if [[ $global_turns -gt 10000 ]]
-then
+# Inform about long processing time
+if [[ $global_turns -gt 10000 ]]; then
 	echo -e "Hinweis:\nDu hast mehr als 10'000 Spiele ausgewählt.\nEs kann länger dauern bis das Programm fertig ist!"
 	echo -e "more than 10000 Turns selected. Proceed with caution." >> "$global_logfile"
 else
 	echo -e "less than 10000 Turns selected. Proceed allowed." >> "$global_logfile"
 fi
-if [[ $global_perfTestDone -eq 1 ]]
-then
+
+# Performance test was done
+if [[ $global_perfTestDone -eq 1 ]]; then
 	echo -e "\nEs wurde ein Performance Test durchgeführt."
-	#calculate eta runtime
+	# Calculate eta runtime
 	global_perfEta=$(
 		awk "BEGIN { print ($global_turns/$perf_betsPerSec) }"
 	)
 	echo -e "Geschätzte Dauer der Durchführung dieses Programmes: ${global_perfEta}s"
-	echo -e "ETA: ${global_perfEta}s" >> $global_logfile
+	echo -e "ETA: ${global_perfEta}s" >> "$global_logfile"
 else
 	echo -e "Es wurde kein Performance Test durchgeführt."
 fi
@@ -91,35 +90,31 @@ read -r -p "Eingabe: " main_mode
 global_betsWon=0
 global_betsTotal=0
 global_runtimeStart=$(date +%s%N)
-if [[ $main_mode -eq 1 ]]
-then
+
+# Write log
+{
+echo "mode chosen: $main_mode"
+echo "global_budget = $global_budget" 
+echo "$global_delimiter" 
+} >> "$global_logfile"
+if [[ $main_mode -eq 1 ]]; then
 	
-	#take input
+	# Take input
 	echo ""
 	read -p "Auf welche Zahl willst du wetten? " global_choice
 
-
-	#write log
-	echo "mode chosen: $main_mode" >> $global_logfile
-	echo "global_budget = $global_budget" >> $global_logfile
-	echo "$global_delimiter" >> $global_logfile
-	
-	#play the game
+	# Play the game
 	func_rouletteLoop
 
-	#end game
+	# End game
 	func_rouletteEnd
-elif [[ $main_mode -eq 2 ]] 
-then
 
-	echo "mode chosen: $main_mode" >> $global_logfile
-	echo "global_budget = $global_budget" >> $global_logfile
-	echo "$global_delimiter" >> $global_logfile
-	global_choice=$(( 36 % 2 ))
-	#play the game
+elif [[ $main_mode -eq 2 ]]; then
+
+	# Play the game
 	func_rouletteLoop
 
-	#end game
+	# End game
 	func_rouletteEnd
 
 else
--- roulette_func.sh
+++ roulette_func.sh
@@ -5,50 +5,71 @@
 
 
 #-------------------------------------------------
-#functions roulette loop
-func_rouletteLoop () {
-{
-echo "START LOOP"
-echo "$global_delimiter"
-echo "chosen values:" 
-echo "global_budget = $global_budget" 
-echo "global_wager = $global_wager" 
-echo "global_turns = $global_turns"
-echo "$global_delimiter" 
-echo "END LOOP"
-} >> "$global_logfile"
+# Functions roulette loop
 
-    while [[ $global_turns -gt 0 ]] 
-	do
-        #echo "test"
-		#reduce turns by 1
+func_rouletteLoop () {
+
+	{
+	echo "START LOOP"
+	echo "$global_delimiter"
+	echo "chosen values:" 
+	echo "global_budget = $global_budget" 
+	echo "global_wager = $global_wager" 
+	echo "global_turns = $global_turns"
+	echo "$global_delimiter" 
+	echo "END LOOP"
+	} >> "$global_logfile"
+
+    while [[ $global_turns -gt 0 ]]; do
+		global_winTrue=0
+		# Reduce turns by 1
 		global_turns=$((global_turns-1))
-		#increase total bets counter to display at end
+		# Increase total bets counter to display at end
 		global_betsTotal=$((global_betsTotal+1))
 
-		#get winning number
-		global_winning=$(( $RANDOM % 36))
-		if [[ $global_winning -eq "${global_choice}" ]]
-		then
-			
-			#add winnings
-			global_budget=$((global_budget+$((global_wager * 35))))
-			#increase won counter to display at end
-			global_betsWon=$((global_betsWon+1))
-			#write log
-			echo "turn $global_betsTotal / won: $((global_wager * 35)) / balance: $global_budget" >> "$global_logfile"
-			echo "$global_delimiter" >> "$global_logfile"
+		printf "\rCurrent Bet: %d | Current Wins: %d" "$global_betsTotal" "$global_betsWon"
+		
 
-		#if budget reaches 0 stop the game
-		else
-			if  [[ $global_budget -gt 0 ]]
-			then
-				global_budget=$((global_budget-global_wager))
-			else
-				global_budget=$((global_budget-0))
-				echo "you lost all your money after $global_betsTotal bets." >> "$global_logfile"
-				break
+		# Get winning number
+		global_winning=$(( $RANDOM % 36))
+
+		# Very slow variant of iterating trough each number
+		#for number in "${global_choice[@]}"; do
+		#	if [[ "$number" -eq "$global_winning" ]]; then
+		#		global_winTrue=1
+		#		break
+		#	fi
+		#done
+		if [[ $main_mode -eq 1 ]]; then
+			if [[ $global_winning -eq $global_choice ]]; then
+				# Add winnings
+				global_budget=$((global_budget+$((global_wager * 35))))
+				# Increase won counter to display at end
+				global_betsWon=$((global_betsWon+1))
+				# Write log
+				#echo "turn $global_betsTotal / winning number: $global_winning / won: $((global_wager * 35)) / balance: $global_budget" >> "$global_logfile"
+				#echo "$global_delimiter" >> "$global_logfile"
 			fi
+		elif [[ $main_mode -eq 2 ]]; then
+			if (( global_winning % 2 == 0 )) && (( global_winning != 0 )); then
+				# Add winnings
+				global_budget=$((global_budget+$((global_wager * 2))))
+				# Increase won counter to display at end
+				global_betsWon=$((global_betsWon+1))
+				# Write log
+				#echo "turn $global_betsTotal / winning number: $global_winning / won: $((global_wager * 35)) / balance: $global_budget" >> "$global_logfile"
+				# Each opening of file 2x the amount of processing
+				#echo "$global_delimiter" >> "$global_logfile"
+			fi
+		fi
+
+		# If budget reaches 0 stop the game
+		if  [[ $global_budget -gt 0 ]]; then
+			global_budget=$((global_budget-global_wager))
+		else
+			global_budget=$((global_budget-0))
+			echo "you lost all your money after $global_betsTotal bets." >> "$global_logfile"
+			break
 		fi
 	done
 }
@@ -71,6 +92,7 @@ func_rouletteEnd (){
 	)
 	
 	#show results
+	echo ""
 	echo "Guthaben: $global_budget"
 	echo "Gewinne: $(($global_budget-$global_budgetOri))"
 	echo "Du hast $global_betsWon von $global_betsTotal Wetten gewonnen. (${global_betsPrct}%)"
@@ -84,8 +106,7 @@ func_rouletteEnd (){
 	echo "Program finished after ${global_runtimeDiffSec}s"
 	} >> "$global_logfile"
 	#if test was done compare to eta time
-	if [[ $global_perfTestDone -eq 1 ]]
-	then
+	if [[ $global_perfTestDone -eq 1 ]]; then
 		#difference from eta to runtime
 		global_runtimeToEta=$(
 		awk "BEGIN { print ($global_runtimeDiffSec-$global_perfEta) }"
